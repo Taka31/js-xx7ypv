e7d754d130c70eead26e6215f5e23afb50465e15
Taka31 <cyrilmenard78@gmail.com>
2023-08-29 17:16:09 +0200

    Training proto 2

---
+++ guitar.js
@@ -0,0 +1,15 @@
+export class Guitar{
+
+  brand;
+
+  constructor(brand="not known"){
+    this.brand=brand;
+  }
+
+  getBrand(){
+    return this.brand;
+  }
+
+
+
+}
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 // Import stylesheets
 import './style.css';
 import { Groupe } from './proto.js';
+import { Guitar } from './guitar.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-Groupe.prototype.setName = (newName) => {
-  groupName = newName;
-};
+
+//------------------- PROTO
+
 Groupe.prototype.MembersCount = 5;
 
 let groupe = new Groupe('Dyouk');
 document.write(groupe.getName() + '<br>');
-document.write(groupe.setName('Rolling Stones') + '<br>');
-document.write(groupe.getName() + '<br>');
 document.write(groupe.MembersCount);
+
+//------------------- Class
+
+let guitar = new Guitar("PRS");
+document.write("<Br>"+guitar.getBrand());
