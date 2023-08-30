fdbe286bf7c02407b05495110d64d17e898ff15e
Taka31 <cyrilmenard78@gmail.com>
2023-08-30 18:54:39 +0200

    codeWars onesAndZero

---
+++ codeWars/onesAndZero.js
@@ -0,0 +1,5 @@
+const binaryArrayToNumber = (arr) => {
+  console.log(parseInt(arr.join(''), 2));
+};
+
+binaryArrayToNumber([1, 1, 1, 1]);
--- index.js
+++ index.js
@@ -5,15 +5,4 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-function swapAdjacentBits(n) {
-  let binary =    n.toString(2).length % 2 === 0 ? n.toString(2) : 0 + n.toString(2);
-  let finalResult = '';
 
-  for (let i = 0; i < binary.length; i += 2) {
-    finalResult += binary[i + 1] + binary[i];
-  }
-
-  return parseInt(finalResult, 2);
-}
-
-swapAdjacentBits(1);
