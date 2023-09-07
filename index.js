65f454e4087da897e16e1133889429cd334b4227
Taka31 <cyrilmenard78@gmail.com>
2023-09-07 12:32:15 +0200

    codeWars sRepeat

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-function findNeedle(haystack) {
- 
-
-  return "found the needle at position "+ haystack.indexOf("needle")
-
-}
-
-console.log(findNeedle(["test","titi","needle"]))
-
