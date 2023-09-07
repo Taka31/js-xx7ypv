fb71b95efcfe20cf3f909a26759e315ea0d6c762
Taka31 <cyrilmenard78@gmail.com>
2023-09-07 12:23:43 +0200

    codeWars findNeedle

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+function findNeedle(haystack) {
+ 
+
+  return "found the needle at position "+ haystack.indexOf("needle")
+
+}
+
+console.log(findNeedle(["test","titi","needle"]))
 
