058f0781819c9560586c3269e1434a5031fe35be
Taka31 <cyrilmenard78@gmail.com>
2023-09-12 13:09:57 +0200

    CodeWars Banjo

--- codeWars/banjo.js
+++ codeWars/banjo.js
@@ -1,10 +1,3 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
 function areYouPlayingBanjo(name) {
   // Implement me
   return name.toUpperCase().substring(0,1)==="R"?name+" plays banjo":name+" does not play banjo";
